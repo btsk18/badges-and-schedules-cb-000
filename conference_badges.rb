730729d0bdd1f43b595d9b2cfc88989a78b6c39a
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 02:08:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 # Write your code here.
 def badge_maker(name)
   puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  assignments = []
+  names.each_with_index { |name, index|
+    assignments << "Hello #{name}! You'll be assigned to room #{index}!"
+  }
+end
