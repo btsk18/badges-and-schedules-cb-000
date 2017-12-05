e0f648a1e9d54a0f607e79628a738e8b0009e242
Brandon Leeds <brandonleeds@gmail.com>
2017-12-05 02:14:43 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(speakers)
   assignments = []
   speakers.each_with_index { |speaker, index|
-    assignments << "Hello #{speaker}! You'll be assigned to room #{index += 1}!"
+    assignments << "Hello, #{speaker}! You'll be assigned to room #{index += 1}!"
   }
   assignments
 end
